d928be4917475a4be15541ac6b584b660d176e2a
gretaivan <greta.ivan91@gmail.com>
2021-07-23 09:44:02 +0100

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double[] leftVals = {100.0, 25.0, 225.0, 11.0};
+        double[] rightVals = {50.0, 92.0, 17.0, 3.0};
+        char[] ops = {'d', 'a', 's', 'm'};
+        
+     
+        System.out.println(Arrays.toString(leftVals));
+        System.out.println(Arrays.toString(rightVals));
+        System.out.println(Arrays.toString(ops));
+	}
+
+}
